3603f4b27dbb8bdbe9d0b6216d8e348a3906625b
Nicholas Zentai <nicholasz8687@gmail.com>
2023-04-10 21:41:52 -0400

    updated syntax in shapes.test.js

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -3,9 +3,9 @@ const shapes = require('./lib/shapes.js');
 describe('square', () => {
     it('should return code that creates a square', () => {
         const userShape = new square();
-        shape.setColor('green');
-        const shape = userChoice.render();
-        expect(shape).toBe(`<rect x="90" y="90" width="100" height="100" fill="green" />`);
+        shape.setColor("blue");
+        const shape = userShape.render();
+        expect(shape).toEqual(`<rect x="90" y="90" width="100" height="100" fill="blue" />`);
     });
 });
 
@@ -13,9 +13,9 @@ describe('square', () => {
 describe('circle', ()=> {
     it('should return code that creates a circle', () => {
         const userShape = new circle();
-        shape.setColor('blue');
+        shape.setColor("blue");
         const shape = userShape.render();
-        expect(shape).toBe(`<circle cx="75" cy="75" r="30" fill="blue" />`);
+        expect(shape).toEqual(`<circle cx="75" cy="75" r="30" fill="blue" />`);
 
     });
 });
@@ -23,8 +23,8 @@ describe('circle', ()=> {
 describe('triangle', () => {
     it('should return code that creates a triangle', () => {
         const userShape = new triangle();
-        shape.setColor('red');
+        shape.setColor("blue");
         const shape = userShape.render();
-        expect(shape).toBe(`<polygon points="150,25 250,150 50,150" fill="red" `);
+        expect(shape).toBe(`<polygon points="150,25 250,150 50,150" fill="blue" `);
     });
 });
